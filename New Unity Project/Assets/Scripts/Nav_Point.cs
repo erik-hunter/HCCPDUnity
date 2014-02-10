ffb26661a5775b86916c683b5e2b9fb584b76485
Sean <sean.callahan@colorado.edu>
2014-02-10 14:20:13 -0700

    Our Project So Far
    This is everything I have done on the project thus far.

---
+++ New Unity Project/Assets/Scripts/Nav_Point.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nav_Point : MonoBehaviour {
+
+    /// <summary>
+    /// This is the unique identifer for each particular node.
+    /// </summary>
+    public int uid;
+
+    public int[] weight = new int[1];
+
+
+    /// <summary>
+    /// Once our Nav_Point falls and lands on the planet, we delete all components 
+    /// except for the transform.
+    /// </summary>
+    /// <param name="other"></param>
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Planet")
+        {
+            Destroy(this.rigidbody);
+            Destroy(this.collider);
+        }
+    }
+
+}
